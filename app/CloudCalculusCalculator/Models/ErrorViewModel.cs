df7c7ed409ddb312c6d01a3f7af767783fb4b3ae
tiredlyrebird <poppers@davies.net.au>
2025-10-17 13:56:21 +1100

    Initial commit

---
+++ app/CloudCalculusCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CloudCalculusCalculator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
